36ca91e2a6fd93f3a16603994513feddb8e5f4a2
Fadlullah <fadlulanre@gmail.com>
2022-10-24 15:12:31 +0100

    restructured codebase

--- app.js
+++ app.js
@@ -1,114 +1,42 @@
 const http = require('http');
-const path = require('path');
 const fs = require('fs');
-const PORT = 3000;
+const port = 3000;
 const hostname = 'localhost';
 
-// createServer is an http method used to create a web server that takes a callback function
+const home = fs.readFileSync("./views/home.html", "utf-8")
+const about = fs.readFileSync("./views/about.html", "utf-8")
+const contact = fs.readFileSync("./views/contact.html", "utf-8")
 
-const httpServer = http.createServer(serverHandler);
+const server = http.createServer(serverHandler);
 
+function serverHandler(req, res) {
 
-  // callback function definition
-
-  function serverHandler(req, res)  {
-
-
-
-
-// Get method for index.html route
-
-    if(req.method === 'GET' && req.url === '/') {
-      let filePath = path.resolve(__dirname, 'routes/index.html')
-      let fileExists = fs.existsSync(filePath);
-
-    // if route doesn't exist, return error...
-      if (!fileExists) {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`
-          <html>
-            <body>
-              <h3>Page not found</h3>
-            </body>
-          </html>`)
-
-          // if route exist, return the specified route.
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream(filePath).pipe(res);
-      }
-    }
-
-    // Get method for about.html route
-    
-    if(req.method === 'GET' && req.url === '/about.html') {
-      let filePath = path.resolve(__dirname, 'routes/about.html')
-      let fileExists = fs.existsSync(filePath);
-      if (!fileExists) {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`
-          <html>
-            <body>
-              <h3>Page not found</h3>
-            </body>
-          </html>`)
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream(filePath).pipe(res);
-      }
-    }
-
-    // Get method for the contact.html route
-
-    if(req.method === 'GET' && req.url === '/contact.html') {
-      let filePath = path.resolve(__dirname, 'routes/contact.html')
-      let fileExists = fs.existsSync(filePath);
-      if (!fileExists) {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`
-          <html>
-            <body>
-              <h3>Page not found</h3>
-            </body>
-          </html>`)
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream(filePath).pipe(res);
-      }
-    }
-
-    // Get method for home.html route
-
-    if(req.method === 'GET' && req.url === '/home.html') {
-      let filePath = path.resolve(__dirname, 'routes/home.html')
-      let fileExists = fs.existsSync(filePath);
-      if (!fileExists) {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`
-          <html>
-            <body>
-              <h3>Page not found</h3>
-            </body>
-          </html>`)
-      } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream(filePath).pipe(res);
-      }
-    }
+  if (req.method == 'GET' && req.url == '/') {
+    res.writeHead(200, { "Content-type": "text/html" })
+    res.end(home)
   }
+  else if (req.method == 'GET' && req.url == '/home') {
+    res.writeHead(200, { "Content-type": "text/html" })
+    res.end(home)
+  }
+  else if (req.method == 'GET' && req.url == '/about') {
+    res.writeHead(200, { "Content-type": "text/html" })
+    res.end(about)
+  }
+  else if (req.method == 'GET' && req.url == '/contact') {
+    res.writeHead(200, { "Content-type": "text/html" })
+    res.end(contact)
+  }
+  else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Portfolio app"
+    })
+    res.end("<h1>Page Not Found</h1>")
+  }
+}
+  
 
-  // httpServer.listen method that takes the PORT number, and hostname already defined, and prints a Server running statement.
-
-  httpServer.listen(PORT, hostname, () => {
-      console.log(`Server running at ${hostname}:${PORT}`);
-
-
-  })
+server.listen(port, hostname, () => {
+  console.log(`Server is listening at ${hostname}:${port}... `);
+})
